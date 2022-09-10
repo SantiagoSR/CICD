b94f6d4bec4c60862fb252056e71990ee912c166
SamtiagoSR <samtiago.santa.ramirez@gmail.com>
2022-09-10 16:14:34 -0500

    Change cuadrado to fail test

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -3,9 +3,9 @@ package co.edu.eafit.protobootapp;
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
-        return numero * numero;
+        return numero + numero;
     }
-    
+
     public Double cubo(Double numero) {
         return numero * numero * numero;
     }
